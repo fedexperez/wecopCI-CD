565a9ea71e92341abf72fdc9ea0a1fb24c84199d
Federico Perez <federicoperez61@gmail.com>
2022-09-18 15:09:34 -0500

    notEcoProduct fix 9

--- docs/SqlScripts/createNotEcoProducts.sql
+++ docs/SqlScripts/createNotEcoProducts.sql
@@ -40,9 +40,9 @@ CREATE TABLE IF NOT EXISTS `not_eco_products` (
 --
 -- Dumping data for table `not_eco_products`
 --
-ALTER TABLE not_eco_products
-  DROP COLUMN id,
-  ADD COLUMN id INT NOT NULL AUTO_INCREMENT PRIMARY KEY;
+ALTER TABLE `not_eco_products`
+  DROP COLUMN (`id`),
+  ADD COLUMN (`id`) INT NOT NULL AUTO_INCREMENT PRIMARY KEY;
 
 INSERT IGNORE INTO `not_eco_products` (`id`, `name`, `price`, `emision`, `product_life`, `created_at`, `updated_at`) VALUES
 (1, 'Common Straw', 0.01, 1.46, 1, '2021-03-23 04:20:10', '2021-03-23 04:20:10'),
